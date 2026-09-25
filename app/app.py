950d22f199c8daba2c50146bc86ef82793acdb72
Om Khare <khareom29@gmail.com>
2026-09-25 08:00:42 +0530

    fix(app): configure MPLCONFIGDIR default for clean containerized startup

--- app/app.py
+++ app/app.py
@@ -13,6 +13,7 @@ import pandas as pd
 import streamlit as st
 
 try:
+    os.environ.setdefault('MPLCONFIGDIR', '/tmp/matplotlib')
     import matplotlib
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
